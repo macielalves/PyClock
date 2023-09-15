2edfa5c9f20e7ed16250c4ed0716b34623fb3263
Maciel Alves Pereira <catce.2023111mtds0003@aluno.ifpi.edu.br>
2023-09-15 19:50:14 -0300

    fix: modificação de menu de configurações

--- clock.py
+++ clock.py
@@ -1,22 +1,31 @@
 import time
+from threading import Thread
+try:
+    from tkinter import Tk, Label
+    GUI = True
+except ImportError:
+    GUI = False
 
 
-class Relogio:
+CMD = '>>> '
+
+
+class Relogio(Thread):
     H = 0
     M = 0
     S = 0
     marca_do_relogio = None
     estado = False
     configurando = False
+    op_span = None
 
     def __init__(self,
                  marca=None,
                  formato=24,
                  horas=0,
                  minutos=0,
-                 segundos=0,
                  estado=False) -> None:
-        """Todos são parâmetros opcionais"""
+        super().__init__()
         self.marca_do_relogio = marca
         self.formato_hora = formato
         self.H = horas
@@ -24,52 +33,92 @@ class Relogio:
         self.estado = estado
         pass
 
-    def ligar(self):
-        self.estado = True
-        return
-
-    def mostrar_tempo(self):
-        print(f'{self.H:0>2}:{self.M:0>2}:{self.S:0>2}')
+    def string_tempo(self):
+        return f'{self.H:02}:{self.M:02}:{self.S:02}'
 
     def atualizar_hora(self):
-        if self.H == self.formato_hora:
-            self.H = 0
-        if self.M == 59:
-            self.H += 1
-            self.M = 0
-        if self.S == 59:
-            self.M += 1
-            self.S = 0
-        self.S += 1
-
-    def configurar(self):
-        self.configurando = True
+        # executa quando estiver com self.estado = True
+        while self.estado:
+            if self.H == self.formato_hora:
+                self.H = 0
+            if self.M == 59:
+                self.H += 1
+                self.M = 0
+            if self.S == 59:
+                self.M += 1
+                self.S = 0
+            self.S += 1
+            time.sleep(1)
 
     def definir_hora(self, hora: int) -> None:
-        if self.configurando:
-            self.H = hora
+        self.H = hora
 
     def definir_minuto(self, minuto: int) -> None:
-        if self.configurando:
-            self.M = minuto
+        self.M = minuto
 
     def definir_segundo(self, segundo: int) -> None:
-        if self.configurando:
-            self.S = segundo
+        self.S = segundo
 
-    def voltar(self):
-        if self.configurando:
-            self.configurando = False
-        pass
+    def configurar(self):
+        self.configurando = True  # ----
+        print('Configuração',
+              '[1] Definir HH:MM',
+              '[2] Modo GUI' if GUI else '',
+              sep="")
+
+        op = input(CMD)
+        if op == '1':
+            print(
+                "[h] h para definir a hora, [m] m para definir minutos, [t] Definir hora e minuto.")
+            sub_op = input(CMD).strip().lower()
+            self.op_span = 'H' if sub_op == 'h' else 'M' if sub_op == 'm' else 'H:M' if sub_op == 't' else None
+            if sub_op and sub_op in 'hmt':
+                tmp = input(f'[{self.op_span}]: ').strip()
+
+        elif op == 'v':
+            print(f'Horário [{self.string_tempo()}]')
+
+    def run(self):  # lista de processos a serem iniciados em thread
+        self.atualizar_hora()
+
+    def stop(self):
+        self.estado = False
+
+    def __str__(self) -> str:
+        return self.string_tempo()
 
 
-r1 = Relogio(formato=12)
-r1.ligar()
-while r1.estado:
-    r1.atualizar_hora()
-    r1.mostrar_tempo()
-    r1.configurar()
-    r1.definir_hora(3)
-    r1.definir_minuto(6)
-    r1.definir_segundo(50)
-    time.sleep(1)
+r1 = Relogio()
+r2 = Relogio()
+
+r1.estado = True
+r1.start()
+r2.estado = True
+r2.start()
+
+try:
+    while True:
+        print(f'Relógio um   [{r1}]\nRelógio dois [{r2}]', end="\r\n\n")
+
+        print(
+            'Digite:'
+            '[1] para configurar o relogio 1, [2] para configurar o relogio 2 ou [q] para sair.'
+        )
+
+        op = input(CMD).strip()
+        if op and op[0].lower() == 'q':
+            break
+
+        if op and op[0] == '1':
+            r1.configurar()
+        elif op and op[0] == '2':
+            r2.configurar()
+
+
+except Exception as err:
+    r1.stop()
+    r2.stop()
+    print(err)
+
+r1.stop()
+r2.stop()
